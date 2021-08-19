0d0151e413664ae66e854752a6ddc018da5d7606
vehikl <go@vehikl.com>
2021-08-19 11:19:59 -0400

    styles for Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Board() {
     return (
-        <div data-testid="board">
-            <div style={{ display: "grid", gridGap: "2px", gridTemplateColumns: "repeat(3, 1fr)" }}>
+        <div data-testid="board" style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <div style={{ display: "inline-grid", gridGap: "2px", gridTemplateColumns: "repeat(3, 1fr)", border: "solid 1px #000" }}>
                 {Array(9).fill(null).map((tile) => (
-                    <div data-testid={'tile'}>x</div>
+                    <div style={{border: "solid 2px #000", height: "100px", width: "100px", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "3rem"}} data-testid={'tile'}>x</div>
                 ))}
             </div>
         </div>
