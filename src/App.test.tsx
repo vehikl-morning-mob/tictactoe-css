21a9e4e929b23a7482ced5b00681f01f887f424d
vehikl <go@vehikl.com>
2021-08-19 10:53:46 -0400

    create 9 tiles for this board

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a blank board to start', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const tiles = screen.getAllByTestId('tile')
+
+  expect(tiles.length).toBe(9)
 });
