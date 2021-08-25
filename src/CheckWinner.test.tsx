0f96d3a11c9c932973870e93500fc3c3274da3b5
vehikl <go@vehikl.com>
2021-08-25 16:26:39 -0400

    Check X winner in all rows

--- src/CheckWinner.test.tsx
+++ src/CheckWinner.test.tsx
@@ -4,4 +4,8 @@ describe('CheckWinner', () => {
     it('returns null if there is no winner', () => {
         expect(checkWinner(['','','','','','','','',''])).toBeNull()
     })
+
+    it.each([[['X','X','X','','','','','','']], [['','','','X','X','X','','','']], [['','','','','','','X','X','X']], ])('X is the winner', (state) => {
+        expect(checkWinner(state)).toBe('X')
+    })
 });
--- src/CheckWinner.ts
+++ src/CheckWinner.ts
@@ -1,4 +1,22 @@
-const checkWinner = () => {
+const checkWinner = (gameState: string[]) => {
+    const allWinners = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        // [0, 3, 6],
+        // [1, 4, 7],
+        // [2, 5, 8],
+        // [0, 4, 8],
+        // [2, 4, 6]
+    ];
+
+    for (let i = 0; i < allWinners.length; i++) {
+        const [a, b, c] = allWinners[i];
+        if (gameState[a] && gameState[a]=== gameState[b] && gameState[a]===gameState[c]) {
+            return `${gameState[a]}`;
+        }
+    }
+
     return null;
 }
 
