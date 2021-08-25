4e27ee5822cbaa38414681b9bc9369140a7ef5be
vehikl <go@vehikl.com>
2021-08-25 15:52:12 -0400

    Announce winner

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,9 @@ import App from './App';
 
 test('renders blank tiles to start', () => {
   render(<App />);
+  const winnerAnnouncement = screen.getByTestId('winner-announcement')
+  expect (winnerAnnouncement.innerHTML).toBe('Game in Progress!')
+
   const tiles = screen.getAllByTestId('tile')
 
   expect(tiles.length).toBe(9)
@@ -36,18 +39,29 @@ test('when the first square is clicked it turns into an X', () => {
 
 test('when the second square is clicked it turns into an O', () => {
   // render app
+
+
   render(<App />)
-  // find the first tile
   const tile = screen.getAllByTestId('tile')[0]
-  // click it
   fireEvent.click(tile)
-  // assert the first tile has innerText of "X"
   expect(tile.innerHTML).toBe("X")
-  // assert all the other tiles have innerText of ""
+
   const tile2 = screen.getAllByTestId('tile')[1]
-  // click it
   fireEvent.click(tile2)
-  // assert the first tile has innerText of "X"
   expect(tile2.innerHTML).toBe("O")
+});
+
+test('when the first row has same symbols, the game is over', () => {
+  // render app
+  render(<App />)
+  const tiles = screen.getAllByTestId('tile')
+  fireEvent.click(tiles[0])
+  fireEvent.click(tiles[3])
+  fireEvent.click(tiles[1])
+  fireEvent.click(tiles[4])
+  fireEvent.click(tiles[2])
+
+  const winnerAnnouncement = screen.getByTestId('winner-announcement')
+  expect (winnerAnnouncement.innerHTML).toBe('X has won!')
 
 });
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,18 +4,25 @@ import Square from "./Square";
 function Board() {
     const [squares, setSquares] = useState(['', '', '', '', '', '', '', '', ''])
     const [nextLetter, setNextLetter] = useState('X');
+    const [winnerAnnouncement, setWinnerAnnouncement] = useState('Game in Progress!');
 
     const handleSquareValue = (index: number) => {
-        let newStateOfSquares = [...squares];
+        let newStateOfSquares = squares;
         newStateOfSquares[index] = nextLetter;
 
         setNextLetter(nextLetter === 'X' ? 'O' : 'X');
         setSquares(newStateOfSquares);
+
+        if (newStateOfSquares[0] === 'X' && newStateOfSquares[1] === 'X' && newStateOfSquares[2] === 'X') {
+            setWinnerAnnouncement('X has won!')
+        }
     }
 
     return (
         <div data-testid="board"
              style={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h1 data-testid="winner-announcement">{winnerAnnouncement}</h1>
+
             <div style={{
                 display: "inline-grid",
                 gridGap: "2px",
